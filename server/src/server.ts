995d4bf6cc12e3431d80e849dd135d5909ef1142
Maycom Willams <74081416+Maycomwill@users.noreply.github.com>
2024-07-11 19:43:05 -0300

    feat: add cors configuration

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,20 @@
 import express from "express";
 import productRouter from "./controllers/products";
+import cors from "cors";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.get("/", (req, res) => {
   res.status(200).send({ message: "Welcome to the API!" });
 });
 
-app.use('/product', productRouter)
+app.use("/products", productRouter);
 
 app.listen(3333, () => {
   console.log("Server is running!");
